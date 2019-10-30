844a50f23c5ebc5d6fae8d13df9a609de69a8aac
Jason Loeffler <j@minorscience.com>
2019-10-30 13:17:42 -0400

    catching up

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
   browserSync: {
-    hostname: "local.sound.local",
+    hostname: "local.sa.local",
     port: 8080,
     openAutomatically: true,
     reloadDelay: 50,
-    injectChanges: true,
+    injectChanges: true
   },
 
   drush: {
     enabled: false,
     alias: {
-      css_js: 'drush @SITE-ALIAS cc css-js',
-      cr: 'drush @SITE-ALIAS cr'
+      css_js: "drush @SITE-ALIAS cc css-js",
+      cr: "drush @SITE-ALIAS cr"
     }
   },
 
